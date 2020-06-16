981a19f6390c348c8992ec186a6fa3e8db138f58
reallyclean-sauce <jerico.arciaga@eee.upd.edu.ph>
2020-06-16 20:49:24 +0800

    Updated the Map Coloring into an OOP code.

--- constraint_programming/map_coloring.py
+++ constraint_programming/map_coloring.py
@@ -1,3 +1,4 @@
+# Implementing MapColoring problem using OOP 
 """
 Problem: Map Coloring
 
@@ -11,119 +12,154 @@ Assign colors to each country while ensuring that no neighboring country has the
 Model:
 Neighbors and Country and Color
 """
-
-# Optimization Problem
-# Decision Variable: Color
-# Constraint: country_color[country] != country_color[neighbors[country]]  for all neighbors
-# Find country color combination when given a single country color
-
 from copy import deepcopy
+from abc import ABCMeta, abstractmethod
 
-#Given:
-colors = ['gray', 'yellow', 'green', 'brown']
-countries = ['Belgium', 'Denmark', 'France', 'Germany',
-			'Netherlands', 'Luxemborg']
-neighbors = {
-	'Belgium': ['France', 'Germany', 'Netherlands', 'Luxemborg'],
-	'Denmark': ['Germany'],
-	'France': ['Belgium', 'Germany', 'Luxemborg'],
-	'Germany': ['Belgium', 'France', 'Luxemborg', 'Netherlands'],
-	'Netherlands': ['Belgium', 'Germany'],
-	'Luxemborg': ['Belgium', 'France', 'Germany']				
-}
+class Given:
+	colors = {'gray', 'yellow', 'green', 'brown'}
+	countries = {'Belgium', 'Denmark', 'France', 'Germany',
+				'Netherlands', 'Luxemborg'}
+	neighbors = {
+		'Belgium': {'France', 'Germany', 'Netherlands', 'Luxemborg'},
+		'Denmark': {'Germany'},
+		'France': {'Belgium', 'Germany', 'Luxemborg'},
+		'Germany': {'Belgium', 'France', 'Luxemborg', 'Netherlands'},
+		'Netherlands': {'Belgium', 'Germany'},
+		'Luxemborg': {'Belgium', 'France', 'Germany'}
+	}
 
-
-# Constraint Programming
-# DS: Search Space / Domain Store for Each Country
-# Constraints: Setting a color for a country
-# Propagation_Engine: Feasibility Checking, Pruning
-
-DS = {}
-for country in countries:
-	DS[country] = deepcopy(colors)
+	def __str__(self):
+		return "Possible Countries are: {}\nPossible Colors are: {}".format(Given.countries,Given.colors)
 
 
-# Assigning color -> Assigning constraint
-country_color = {}
-for country in countries:
-	country_color[country] = None
+class DomainStore(Given):
+	"""Search Space
+	for all the possible decision variables
+	for all the countries
+	"""
+	search_space = {}
+	for country in Given.countries:
+		search_space[country] = deepcopy(Given.colors)
 
-# Basic Propagation engine
-while True:
-	# Search Store
-	# Sending constraints to Constraint Store
-	chosen_country = input("Choose country: ")
-	if chosen_country not in countries:
-		print("Invalid country")
-		continue
 
-	mode = input("Choose Mode: ")
-	
-	country_color[chosen_country] = input("Choose color: ")
-	if country_color[chosen_country] not in colors:
-		print("Invalid color")
-		country_color[chosen_country] = None
-		continue
+	def showSearchSpace(self):
+		for country in Given.countries:
+			print(country, DomainStore.search_space[country])
 
 
 
-	# Constraint Store
-	
-	# Feasibility Checking
-	# Check if constraint will find a solution
-	feasible = True
-	for country in countries:
+class PropagationEngine(DomainStore):
+	def feasibility_check(self,country):
+		"""Feasibility Checking
+		Check if constraint will find a solution
+		"""
+		feasible = True
+		for c in Given.countries:
+			for n in Given.neighbors[c]:
+				# print(type(DomainStore.search_space[c]),c)
+				# print(type(DomainStore.search_space[n]),n)
+				if len(DomainStore.search_space[c] | DomainStore.search_space[n]) == 1:
+					feasible = False
+					print(DomainStore.search_space[c],c)
+					print(DomainStore.search_space[n],n)
 
-		if country_color[country] is None:
+		return feasible
+
+	def prune(metaclass=ABCMeta): pass
+
+
+	def prunability(self):
+		"""Check for Prunability
+		Checks if we have found a solution
+		"""
+		solution = True
+		for c in Given.countries:
+			for n in Given.neighbors[c]:
+				if (len(DomainStore.search_space[c] | DomainStore.search_space[n]) != 2) and \
+					(len(DomainStore.search_space[c] & DomainStore.search_space[n]) == 0):
+					solution = False
+
+		return solution
+
+
+	def propagate(self,country,color):
+		"""Perform Propagation
+		"""
+		self.prune(country,color)
+
+
+		if not self.feasibility_check(country):
+			raise Exception("Constraint is not feasible!")
+
+		result = self.prunability()
+		if result:
+			print("Solution")
+			self.showSearchSpace()
+			return True
+		else:
+			print("Current Domain Space")
+			self.showSearchSpace()
+			return False
+			
+
+
+class AssignColor(PropagationEngine):
+	def prune(self,country,color):
+		"""Prunes the DomainStore
+		when a Color is assigned to a Country
+		"""
+
+		# Reduce DomainSpace to 1 of the Country
+		DomainStore.search_space[country] = {color}
+
+		# Prune the DomainSpace according to the model
+		for neighbor in Given.neighbors[country]:
+			if color in DomainStore.search_space[neighbor]:
+				DomainStore.search_space[neighbor].discard(color)
+
+				# Prune if a country has only one choice left
+				if len(DomainStore.search_space[neighbor]) == 1:
+					for n in Given.neighbors[neighbor]:
+						for neighbor_color in DomainStore.search_space[neighbor]: pass
+
+						if neighbor_color in DomainStore.search_space[n]:
+							DomainStore.search_space[n].discard(neighbor_color)
+
+
+
+class PreventColor(PropagationEngine):
+	def prune(self,country,color):
+		"""Prunes the DomainStore
+		when a Color is not allowed to be a value for the Country
+		"""
+		print("PREVENTING:!!")
+		print(DomainStore.search_space[country])
+		DomainStore.search_space[country].discard(color)
+		print(DomainStore.search_space[country])
+
+		
+if __name__ == "__main__":
+	p = {10,12}
+	print(p)
+
+	assign_color = AssignColor()
+	prevent_color = PreventColor()
+	given = Given()
+	while True: 
+		print(given)
+		country = input("Choose a Country: ")
+		color = input("Choose a Color: ")
+		mode = input("Choose mode:")
+		print("\n\n\n\n\n\n\n\n")
+		if mode == 'a':
+			result = assign_color.propagate(country,color)
+		elif mode == 'p':
+			result = prevent_color.propagate(country,color)
+		else:
+			print("Invalid mode")
 			continue
 
-		for neighbor in neighbors[country]:
-			if country_color[country] == country_color[neighbor]:
-				feasible = False
+		if result:
+			break
 
-	if not feasible:
-		print("Constraint is not feasible!")
-		print("Choose another constraint")
-		country_color[chosen_country] = None
-		break
-
-
-	# Pruning Algorithm
-	# Two types of Pruning Algorithm:
-	# # Assigning a color for a country
-	# # Preventing a color to be assigned to a country
-	if mode == 'a':
-		DS[chosen_country] = [country_color[chosen_country]]
-		for neighbor in neighbors[chosen_country]:
-			if country_color[chosen_country] in DS[neighbor]:
-				DS[neighbor].remove(country_color[chosen_country])
-	elif mode == 'p':
-		DS[chosen_country].remove(country_color[chosen_country])
-	else:
-		print("Chosen mode is not available")
-
-	for country in countries:
-		if len(DS[country]) == 1:
-			country_color[country] = DS[country][0]
-			for neighbor in neighbors[country]:
-				if country_color[country] in DS[neighbor]:
-					DS[neighbor].remove(country_color[country])
-
-
-	# Check if there are any values left to be pruned
-	count = 0
-	for country in DS:
-		if len(DS[country]) == 1:
-			count += 1
-
-	if count == len(DS.keys()):
-		print("Color Combination chosen:")
-		for country in countries:
-			print(country, country_color[country])
-		break
-
-
-	print("New Domain Store after setting France to Gray")
-	for country in DS:
-		print(country)
-		print(DS[country])
+	
